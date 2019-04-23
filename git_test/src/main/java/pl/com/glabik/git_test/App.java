395276a28c95f6877b848253bb2e07d6d93dd90e
glabik <42345502+glabik@users.noreply.github.com>
2019-04-23 11:01:42 +0200

    Update App.java
    test

--- git_test/src/main/java/pl/com/glabik/git_test/App.java
+++ git_test/src/main/java/pl/com/glabik/git_test/App.java
@@ -7,7 +7,7 @@ package pl.com.glabik.git_test;
 public class App {
 	
     public static void main(String[] args){
-        System.out.println("Hello World 1 !");
+        System.out.println("Hello World 2 !");
     }
 	
 }
